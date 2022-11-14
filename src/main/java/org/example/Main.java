191305090e79c566e79bf0f489a23527eca01a0d
Haseeb <haseeb091097@gmail.com>
2022-11-14 15:49:20 +0000

    started implemntation of user input to do keep looping till game over

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,35 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Grid grid=new Grid(4,4);
+
+        int row;
+        int col;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Number of rows");
+        while (!myObj.hasNextInt()){
+            myObj.next();
+            System.out.println("Please enter a valid  Number of rows");
+        }
+        row=myObj.nextInt();
+        System.out.println("Enter Number of columns");
+        while (!myObj.hasNextInt()){
+            myObj.next();
+            System.out.println("Please enter a valid  Number of columns");
+        }
+        col=myObj.nextInt();
+        Grid grid=new Grid(row,col);
         grid.printAll();
+
+        //first print grid then x any why cordinates
+        //verify cordinates
+        // make points visible
+        //send to grid to check if its a mine
+            //exit
+        //loop unitl empty is 0
+        
+
     }
 }
